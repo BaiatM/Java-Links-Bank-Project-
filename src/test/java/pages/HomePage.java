af397f34ca16ec9ec8c873b2d37cd976c6cf1a0b
Kamird <dilmuradovna@gmail.com>
2023-02-09 21:05:03 -0600

    Profile buttons verification

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,11 +5,25 @@ import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
 //16b. create verification of HomePage
-public class HomePage extends BasePage{
+public class HomePage extends BasePage {
     @FindBy(xpath = "//li[@class=\"active\"]")
     WebElement welcomeName;
+    @FindBy(xpath = "//img[@alt='User Avatar']")
+    WebElement profileDropDown;
+    @FindBy(xpath = "//*[text()='My Profile']")
+   public  WebElement profile;
+    @FindBy(xpath = "//*[text()='Change Password']")
+   public  WebElement changePassword;
+    @FindBy(xpath = "//*[text()='Logout']")
+    public WebElement logOut;
 
-    public void verifyHomePage(){
+    public void verifyHomePage() {
         Assert.assertTrue(welcomeName.isDisplayed(), "You are not on the homepage; missing \"Welcome\" message");
     }
+    public void clickOnAccountBtn(){
+        profileDropDown.click();
+    }
+
+
+
 }
