63b4d021a5e8e41d9bb39e521ae924ee7a448848
Baiat <baiat0215@gmail.com>
2023-03-10 23:05:35 -0500

    Added arguments which allow to use newer version of Chrome for mac users

--- src/test/java/utils/DriverUtils.java
+++ src/test/java/utils/DriverUtils.java
@@ -3,6 +3,7 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,9 +12,11 @@ public class DriverUtils {
 
     public static void createDriver(){
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("http://18.118.14.155:8080/bank/login");
     }
 
